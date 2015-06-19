5c9e821887573d403086ed07c66f83f821266466
Dzung Tran <tiendung@paymentwall.com>
2015-06-19 12:00:13 +0700

    Update for VM 3

--- src/paymentwall.php
+++ src/paymentwall.php
@@ -7,6 +7,23 @@
  * @subpackage payment
  * @copyright Copyright (C) 2010-2014 Paymentwall Inc.
  * @license The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -62,7 +79,12 @@ class plgVmpaymentPaymentwall extends vmPSPlugin
    */
   function plgVmOnStoreInstallPaymentPluginTable ($jplugin_id)
   {
-    return $this->onStoreInstallPaymentPluginTable($jplugin_id);
+      if(!defined('VM_VERSION') or VM_VERSION < 3){
+          // for older vm version
+          return $this->onStoreInstallPaymentPluginTable($jplugin_id);
+      }else{
+          return $this->onStoreInstallPluginTable($jplugin_id);
+      }
   }
 
   /**
@@ -225,6 +247,15 @@ class plgVmpaymentPaymentwall extends vmPSPlugin
   {
     return $this->setOnTablePluginParams($name, $id, $table);
   }
+
+    /**
+     * Addition triggers for VM3
+     * @param $data
+     * @return bool
+     */
+    function plgVmDeclarePluginParamsPaymentVM3( &$data) {
+        return $this->declarePluginParams('payment', $data);
+    }
 
   /**
    * This method is fired when showing when priting an Order
